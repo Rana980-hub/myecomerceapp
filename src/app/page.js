c7eeedb71824f6450d4360d5ccf5b9d6d2227b79
Rana980-hub <harr91055@gmail.com>
2024-10-08 01:54:29 -0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
 "use client";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { useEffect, useState } from 'react';
 import Bar from './nav/Barr';
 
